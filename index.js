38ecbf99c37a87871a7b7ac48c340429be555804
Dharm Joshi <dharm.j@applify.co>
2025-05-03 17:01:26 +0530

    added a new route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res)=> {
+    console.log("Hello this is the new route");
+    res.send("New route added")
+})
 app.listen(3000, ()=> {
     console.log("server is listening on the port 3000")
 });
